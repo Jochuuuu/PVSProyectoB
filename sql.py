a5fd64f2ff892391cf5f6144640a84398799bb60
Jochuuuu <josebm919@gmail.com>
2025-11-14 15:29:10 -0500

    style: format code with black locally

--- sql.py
+++ sql.py
@@ -1105,7 +1105,7 @@ class SQLTableManager:
         print(f"Analizando WHERE clause: '{where_clause}'")
 
         # 1. Primero extraer condiciones BETWEEN
-        between_pattern = r"(\w+)\s+BETWEEN\s+(.+?)\s+AND\s+(.+?)(?:\s+AND\s+|$)"
+        between_pattern = r"(\w+)\s+BETWEEN\s+(\S+)\s+AND\s+(\S+)"
         between_matches = list(re.finditer(between_pattern, where_clause, re.IGNORECASE))
 
         remaining_clause = where_clause
